340aa5de4509abe1e4bc26003303555f54cb866a
yasglobe <127344117+yasglobe@users.noreply.github.com>
2023-03-11 16:54:03 +0300

    Add files via upload

---
+++ Python-Selenium.py
@@ -0,0 +1,38 @@
+# -> string(str) = Metinsel veri tipidir.
+# -> integer(int) = Tam sayıları ifade eden veri tipidir.
+# -> float(float) = Ondalıklı sayıları ifade eden veri tipidir.
+# -> boolean(bool) = Mantıksal veri tipidir. True ya da false çıktısını verir.
+#Kodlama.io sitesinde genel olarak string(str) veri tipinin kullanıldığını gördüm.
+
+#string veri tipine örnek.
+str = "Selam"
+print(str)
+str1 = "Naber?"
+print(str1)
+#Kodlama.io sitesinde de kurs başlığı olarak gördüğümüz kurs başlıkları da string veri tipine örnektir.
+
+#Rakamsal veri tipine örnek.
+num = 3
+print(num)
+num1 = 5.5
+print(num1)
+# -> Burada tanımlanan değişken yani rakamsal veri tipine örnektir.
+#Kodlama.io sitesinde bulunan dersin ilerleme oranını gösterir.
+
+#Kodlama.io sitesinde kullanılan şart blokları şu şekildedir.
+saat1 = 9
+saat2 = 8
+saat3 = 12
+
+if saat1 > saat2:
+    print("Ders başlamadı.")
+
+elif saat1 == saat2:
+    print("Ders başladı.")
+
+else:
+    print("Ders bitti.")
+
+
+
+
