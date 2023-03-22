d06941d1f22d855f5340488bc5d167f87088d78a
yasglobe <127344117+yasglobe@users.noreply.github.com>
2023-03-22 18:28:45 +0300

    Create python ödev3.py

---
+++ python ödev3.py
@@ -0,0 +1,58 @@
+""""
+HTML Nedir?
+
+-> Web sayfalarını oluşturma aşamasında kullanılan standart bir metin işaret dilidir.HTML, 
+web tasarımcılarına sayfalar ve uygulamalar için yapı profilleri, bağlantılar, blok 
+alıntılar, paragraflar ve başlıklar oluşturmalarında yardımcıdır.
+
+
+-----------------------------------------------------------------------------------------
+
+
+En Çok Kullanılan HTML Kodları:
+
+* <!DOCTYPE html>: Tüm HTML belgeleri bu kod ile başlamak zorundadır. Bu kod aslında sayfanın 
+HTML5 formatına göre kodlandığını gösterir.
+
+* <html>: Kök etiket olarak da bilinen bu temel kullanımda tüm HTML kodları, bu iki etiket 
+arasında tanımlanmak zorundadır.
+
+* <body>: Görünebilir tüm HTML kod parçalarının eklendiği etiket alanıdır. 
+
+* <head>: Web sayfasının dili, başlığı ve diğer düzenlemelerinin yer aldığı etikettir.
+
+*<title>: Web sitesinin başlık alanını tanımlamak için kullanılır. <title> etiketi, <head>
+etiketleri arasında olmak zorundadır.
+
+* <meta charset=”utf-8″>: HTML kodunun Türkçe karakter desteği dahil olmak üzere pek çok dil 
+setiiçerdiğini belirtir.
+
+HTML Locators Nedir?
+
+-> Locators(Konumlandırıcı), Selenium IDE'ye hangi web tabanlı objeler üzerinde çalışması gerektiğini
+söyleyen bir komuttur. Doğru elementin tanımlanması, otomasyon oluşturmanın ön koşuludur.
+
+Bazı Locators çeşitleri:
+* ID
+* Name
+* ClassName
+* TagName
+* LinkText
+* CssSelector
+* XPath
+
+
+-----------------------------------------------------------------------------------------
+
+
+Seleniumda aksiyonlar:
+
+* quit() ->  Açık olan tüm sayfaları kapatır.
+* close() -> Sadece en sonda açılmış olan sayfayı kapatır. 
+* maximize_window() -> Tarayıcıyı tam ekran yapmak için  kullanılır.
+* refresh() -> Sayfayı yenilemek için kullanılır.
+* forward() --> Bir sonraki sayfaya gider.
+* back() --> Bir önceki sayfaya gider.
+
+Not: Çok daha fazla fonksiyon vardır ancak en çok kullandıklarım ve aklımda kalanları yazdım. 
+"""
